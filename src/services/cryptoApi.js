d5cd6b0bde87d46d70fa85c0eaac3a92fbfea736
Your Name <you@example.com>
2021-10-01 00:54:19 -0400

    added content to cryptoDetails component

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,6 +16,9 @@ export const cryptoApi = createApi({
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
         })
     })
 });
@@ -23,4 +26,5 @@ export const cryptoApi = createApi({
 // note for how this final hook is generated https://redux-toolkit.js.org/rtk-query/overview
 export const {
     useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
 } = cryptoApi;
