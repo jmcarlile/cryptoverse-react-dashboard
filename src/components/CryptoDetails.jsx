8a9872a7f5dc9a9ec4b121ea2a1a3a4be478622c
Your Name <you@example.com>
2021-10-01 01:05:29 -0400

    added redux query

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -6,6 +6,7 @@ import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
 import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
+import LineChart from './LineChart';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -57,7 +58,13 @@ const CryptoDetails = () => {
                         {time.map((date) => (<Option key={date}>{date}</Option>))}
 
                 </Select>
-                {/* line chart */}
+                
+                <LineChart 
+                    coinHistory={coinHistory} 
+                    currentPrice={millify(cryptoDetails.price)} 
+                    coinName={CryptoDetails.name} 
+                />
+
                 <Col className="stats-container">
                     <Col className="coin-value-statistics">
                         <Col className="coin-value-statistics-heading">
