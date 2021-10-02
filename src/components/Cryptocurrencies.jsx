f887062404817d6b931a5c4b1a9e564079e356dc
Your Name <you@example.com>
2021-10-02 15:00:59 -0400

    added alt tag to image

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -36,7 +36,7 @@ const Cryptocurrencies = ({ simplified }) => {
                     <Link to={`/crypto/${currency.id}`}>
                         <Card 
                             title={`${currency.rank}. ${currency.name}`}
-                            extra={<img className="crypto-image" src={currency.iconUrl} />}
+                            extra={<img className="crypto-image" src={currency.iconUrl} alt={currency.name} />}
                             hoverable
                         >
                             <p>Price: {millify(currency.price)}</p>
